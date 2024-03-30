33331bac9706a414e2fbf50146080874ccadc1a0
Daniel <danielbrodowski97@gmail.com>
2024-03-30 14:11:23 +0000

    added scroller
    aside scroller with dev tags added

--- js.js
+++ js.js
@@ -1,7 +1,31 @@
-
-
 const headerEl = document.getElementById("header");
 
-function toggleMenu(){
-    headerEl.classList.toggle("nav-open");
+function toggleMenu() {
+  headerEl.classList.toggle("nav-open");
 }
+
+// SCROLLER
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduced)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+    });
+
+  });
+}
+
+
